21e8dd545888648ce3c85572d8b725f3ccc8f04b
Patrick Mazzuco <patrick.flores@acad.pucrs.br>
2021-07-10 01:07:35 -0300

    feat: update SideBar and Content components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,59 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import { api } from '../services/api';
+import { Button } from './Button';
+import { GenreResponseProps } from '../App';
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  selectedGenreId: number;
+  onSelectedGenreIdChange: (genreId: number) => void;
+  onSelectedGenreChange: (genre: GenreResponseProps) => void;
+}
+
+export function SideBar({
+  selectedGenreId,
+  onSelectedGenreIdChange,
+  onSelectedGenreChange,
+}: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api
+      .get<GenreResponseProps>(`genres/${selectedGenreId}`)
+      .then((response) => {
+        onSelectedGenreChange(response.data);
+      });
+  }, [selectedGenreId]);
+
+  function handleGenreChange(genreId: number) {
+    onSelectedGenreIdChange(genreId);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleGenreChange(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
